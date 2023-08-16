5c904c58c80f56507e33f3428814913460f5c738
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-16 18:41:46 +0545

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     }
 }
 
+SUPABASE_URL = config('SUPABASE_URL')
+SUPABASE_KEY = config('SUPABASE_KEY')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
