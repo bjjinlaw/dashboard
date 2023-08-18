3cab26d3ba2318c46712e320230b066ac395122a
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-19 00:53:34 +0545

    first database test

--- dashapp/admin.py
+++ dashapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from dashapp.models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "sku")
+    search_fields = ("name", "sku")
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
+
 DATABASES['default'] = dj_database_url.config()
 
 
