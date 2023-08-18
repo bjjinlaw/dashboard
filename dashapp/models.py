cf4e5c50e233551153587f7dd553966fdfcc4a4b
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-19 01:38:06 +0545

    Update models.py

--- dashapp/models.py
+++ dashapp/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=20, decimal_places=2)
     sku = models.CharField(max_length=255, unique=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    description = models.CharField(max_length=255)
     
     def __str__(self):
         return self.name
