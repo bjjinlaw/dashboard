98168a79c7603d0cef83899baf47a250a295a44e
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-19 01:30:59 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -11,6 +11,6 @@ echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
 
 # createsuperuser
-python3.9 manage.py createsuperuser
+python3.9 manage.py createsuperuser --noinput
 --username demo
 --password demo
