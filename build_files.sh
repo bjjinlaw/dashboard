e8e304881d9920aa40ab8ae3a6dc7f53bec8eb95
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-19 01:20:06 +0545

    after superuser try

--- build_files.sh
+++ build_files.sh
@@ -9,3 +9,15 @@ python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
+
+# createsuperuser
+python3.9 manage.py createsuperuser
+Username: demo
+Email address:
+Password: demo
+Password (again): demo
+The password is too similar to the username.
+This password is too short. It must contain at least 8 characters.
+This password is too common.
+Bypass password validation and create user anyway? [y/N]: y
+Superuser created successfully.
