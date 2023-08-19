d3022b5361465e8fbdc37bc76da62752a21fe0e9
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-19 09:11:12 +0545

    delete

--- dashapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from dashapp.models import Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "sku")
-    search_fields = ("name", "sku")
