14e45fd52c78ab98417a43279b111ddec3d9b44d
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-20 16:53:57 +0545

    Update urls.py

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -7,5 +7,4 @@ app_name="user"
 urlpatterns = [
     path("register/",UserRegisterView.as_view(),name="register/"),
     path("update-delete-user/<int:pk>/",UserDetailUpdateDeleteView.as_view(),name="update-delete/"),
-    path("superuser/",CreateSuperUser.as_view())
 ]
