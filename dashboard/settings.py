6d695243dd6f42a4f4f3f61e4f449b37a1150581
bjjinlaw <123348980+bjjinlaw@users.noreply.github.com>
2023-08-20 17:36:02 +0545

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 # # Check if the environment variable is set for online deployment
 # if "ONLINE_DEPLOYMENT" in os.environ:
-#     # Use dj_database_url to configure the database from the environment variable
+# Use dj_database_url to configure the database from the environment variable
 
 DATABASES['default'] = dj_database_url.config()
 
